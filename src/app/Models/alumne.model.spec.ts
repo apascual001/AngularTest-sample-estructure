c4ae1d444498656936b7b31948724d2398d48adb
Ana Pascual <annapascual001@gmail.com>
2020-03-07 01:04:51 +0100

    Alumne model

---
+++ src/app/Models/alumne.model.spec.ts
@@ -0,0 +1,7 @@
+import { Alumne.Model } from './alumne.model';
+
+describe('Alumne.Model', () => {
+  it('should create an instance', () => {
+    expect(new Alumne.Model()).toBeTruthy();
+  });
+});
