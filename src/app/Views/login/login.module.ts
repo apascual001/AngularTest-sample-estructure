6713ada7c58656a9a383f0583ef7fef416718e46
jgverges <55912813+jgverges@users.noreply.github.com>
2020-03-07 18:38:05 +0100

    comencem amb login

--- src/app/Views/login/login.module.ts
+++ src/app/Views/login/login.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormBuilder,FormGroup,FormsModule,Validators} from '@angular/forms';
+
 
 import { LoginRoutingModule } from './login-routing.module';
-
+import {LoginComponent} from './login/login.component'
+import {NewPasswordResetComponent} from './new-password-reset/new-password-reset.component';
+import {ResetPasswordRequestComponent} from './reset-password-request/reset-password-request.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent,NewPasswordResetComponent,ResetPasswordRequestComponent],
   imports: [
     CommonModule,
+    FormBuilder,FormGroup,FormsModule,Validators,
     LoginRoutingModule
   ]
 })
