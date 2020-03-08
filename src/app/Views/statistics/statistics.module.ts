d1105f472202e8c545ae48163e6663b76ef1c510
jgverges <55912813+jgverges@users.noreply.github.com>
2020-03-08 06:41:52 +0100

    eliminamos statistics-view para conservar stadistics del proyecto original

--- src/app/Views/statistics/statistics.module.ts
+++ src/app/Views/statistics/statistics.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StatisticsRoutingModule } from './statistics-routing.module';
-import { StatisticsViewComponent } from './statistics-view/statistics-view.component';
 import { StadisticsComponent } from './stadistics/stadistics.component';
 import { PeoplepieComponent } from './stadistics/peoplepie/peoplepie.component';
 import { ChartsModule } from 'ng2-charts';
@@ -10,7 +9,7 @@ import { GenrepieComponent } from './stadistics/genrepie/genrepie.component';
 import {TablesComponent} from './tables/tables.component'
 
 @NgModule({
-  declarations: [StatisticsViewComponent, StadisticsComponent, PeoplepieComponent, GenrepieComponent,TablesComponent],
+  declarations: [ StadisticsComponent, PeoplepieComponent, GenrepieComponent,TablesComponent],
   imports: [
     CommonModule,
     ChartsModule,
