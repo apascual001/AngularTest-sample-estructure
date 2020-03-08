96fa29dd5e64845fbb8a03b007e3979c19905d41
Ana Pascual <annapascual001@gmail.com>
2020-03-08 23:29:20 +0100

    Classroom module - Services, imports and providers

--- src/app/Views/classroom/classroom.module.ts
+++ src/app/Views/classroom/classroom.module.ts
@@ -6,20 +6,37 @@ import { ClassroomViewComponent } from './classroom-view/classroom-view.componen
 import { ClassroomSeatsComponent } from './classroom-seats/classroom-seats.component';
 import { StudentPositionComponent } from './student-position/student-position.component';
 
+// Services
+import { ClassroomService } from '../../Services/classroom.service';
+//import { StudentSearchService } from 'src/app/services/student-search.service';
+
 // graficos
 import { ChartsModule } from 'ng2-charts';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { StudentPopupComponent } from './student-popup/student-popup.component';
 
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 @NgModule({
-  declarations: [ClassroomViewComponent, ClassroomSeatsComponent, StudentPositionComponent, StudentPopupComponent],
+  declarations: [
+    ClassroomViewComponent,
+    ClassroomSeatsComponent, 
+    StudentPositionComponent, 
+    StudentPopupComponent
+  ],
   imports: [
     CommonModule,
     ChartsModule,
     TooltipModule.forRoot(),
     NgCircleProgressModule.forRoot({radius: 20, showBackground: true}),
+    ModalModule.forRoot(),
     ClassroomRoutingModule
+  ],
+  providers: [
+    ClassroomService,
+    //AlumnoService,
+    //StudentSearchService
   ]
 })
 export class ClassroomModule { }
