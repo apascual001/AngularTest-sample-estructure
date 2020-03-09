03b5d413281ea6451fab717cd29da86702fc5bab
jgverges <55912813+jgverges@users.noreply.github.com>
2020-03-09 18:37:53 +0100

    elimino duplicados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,11 @@ import { StudentFileModule } from './Views/student-file/student-file.module';
 import { StatisticsModule } from './Views/statistics/statistics.module';
 import { ReviewsModule } from './Views/reviews/reviews.module';
 import { ClassroomModule } from './Views/classroom/classroom.module';
-import { LoginModule } from './Views/login/login.module';
 /* components */
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { LayoutComponent } from './Components/layout/layout.component';
 import { LoginModule } from './Views/login/login.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
