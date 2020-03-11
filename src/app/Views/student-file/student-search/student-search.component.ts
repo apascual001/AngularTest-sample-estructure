e578d6568200a8894348fb299870419dc7869752
Ana Pascual <annapascual001@gmail.com>
2020-03-11 12:10:23 +0100

    student-search.service route updated

--- src/app/Views/student-file/student-search/student-search.component.ts
+++ src/app/Views/student-file/student-search/student-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentSearch } from '../../../models/student-search';
-import { StudentSearchService } from '../../../services/student-search.service';
+import { StudentSearchService } from '../../../Services/student-search.service';
 import { Select2OptionData } from 'ng2-select2';
 import { Router } from '@angular/router';
 /* ? */
