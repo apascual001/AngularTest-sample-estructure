218d2a7ae1761fe9961ddc44d80535089e429022
Ana Pascual <annapascual001@gmail.com>
2020-03-14 12:30:53 +0100

    Adding components and services to student-file module

--- src/app/Views/student-file/student-file.module.ts
+++ src/app/Views/student-file/student-file.module.ts
@@ -3,13 +3,35 @@ import { CommonModule } from '@angular/common';
 
 /* components & modules */
 import { StudentFileRoutingModule } from './student-file-routing.module';
-import { StudentComponent} from './student/student.component'
+import { StudentComponent} from './student/student.component';
+import { StudentSearchComponent} from './student/student-search/student-search.component';
+import { FileStudentComponent } from './student/file-student/file-student.component';
+import { ExerciseModalComponent } from './student/exercise-modal/exercise-modal.component';
+import { ExercisesComponent } from './student/exercises/exercises.component';
+import { TablesComponent } from './student/tables/tables.component';
+import { TimeBarComponent } from './student/time-bar/time-bar.component';
+
+/* services*/
+import { StudentSearchService } from '../../Services/student-search.service';
+import { Select2Module } from 'ng2-select2';
 
 @NgModule({
-  declarations: [StudentComponent],
+  declarations: [
+    StudentComponent,
+    StudentSearchComponent,
+    FileStudentComponent,
+    ExerciseModalComponent,
+    ExercisesComponent,
+    TablesComponent,
+    TimeBarComponent
+  ],
   imports: [
     CommonModule,
+    Select2Module,
     StudentFileRoutingModule
+  ],
+  providers: [
+    StudentSearchService
   ]
 })
 export class StudentFileModule { }
